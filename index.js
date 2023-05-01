08b2c74806854962ffceb28198b83335b615e606
Ben <ben.j.marum@gmail.com>
2023-05-01 16:50:38 -0600

    Completed Lab

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+
+// function multiply(num1,num2){
+//     return num1*num2;
+// }
+
+let num1 = 31;
+let num2 = 2;
+let num3 = 24;
+let num4 = 5;
+
+let multiply = num1*num2;
+let random = Math.ceil(Math.random()*10);
+let mod = num3 % num4;
+let max = Math.max(1,2,3,4,5,20);
+
